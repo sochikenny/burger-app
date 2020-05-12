31d94392a120463a37afb256351f2ed04e5bfd02
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-05-11 22:18:59 -0400

    slight change to the connection file based on Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
