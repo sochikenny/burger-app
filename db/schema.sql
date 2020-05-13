b803272fc6a0d2a50ac3ba91821e9c838a945cad
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-05-12 20:09:41 -0400

    cleaning things up

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,9 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE database burgers_db;
-
-USE burgers_db;
 
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(225) NOT NULL,
   devoured BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("cheeseburger", true),
-       ("Bacon cheeseburger", true),
-       ("Veggie burger", false);
-       
-SELECT * FROM burgers; 
